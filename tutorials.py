939991d0acab188f20dad271ae188b196804ff74
kaeini <spiderdan19@gmail.com>
2024-09-30 14:22:10 +0100

    Forgot to add other changes oops

--- tutorials.py
+++ tutorials.py
@@ -10,6 +10,10 @@ class Tutorials(discord.ui.View):
         max_values=1,
         options=[
             discord.SelectOption(
+                label="CO1102",
+                description="Python Fundamentals"
+            ),
+            discord.SelectOption(
                 label="CO1105",
                 description="Object Oriented Programming"
             ),
@@ -34,7 +38,20 @@ class Tutorials(discord.ui.View):
     )
     async def on_select(self, select, interaction):
         embed = discord.Embed(title=f"Tutorial for {select.values[0]}", description="Here are some helpful tutorials/materials for this module", color=discord.Color.random())
-        if select.values[0] == "CO1105":
+        if select.values[0] == "CO1102":
+            embed.set_image(url="https://cdn.freebiesupply.com/logos/large/2x/python-5-logo-png-transparent.png")
+            embed.add_field(name="Week 1", value="[Playlist](https://youtube.com/playlist?list=PLeVt6bfkArKeUHaPvDZIONynfnj3oyN5Y&si=5bIoxejRgdAEYoam)", inline=True)
+            embed.add_field(name="Week 2", value="[Playlist](https://www.youtube.com/playlist?list=PLeVt6bfkArKcWE9I9wZvv4Bbyd4e0Nvst)", inline=True)
+            embed.add_field(name="Week 3", value="[Playlist](https://www.youtube.com/playlist?list=PLeVt6bfkArKcLNCjLF0wUfWsoH64CtTfh&jct=ODlb5FmGrOHWXxwV4QyV3A)", inline=True)
+            embed.add_field(name="Week 4", value="[Playlist](https://youtube.com/playlist?list=PLeVt6bfkArKdmYvuMkYPaP3eeLpq6OPGt&si=5puLw5u2ZjhFC17T)", inline=True)
+            embed.add_field(name="Week 5", value="[Video](https://youtu.be/621C1FvvcCc?list=PLeVt6bfkArKc9GvP81JdNMw6j4K1BmttN)", inline=True)
+            embed.add_field(name="Week 6", value="[Playlist](https://www.youtube.com/playlist?list=PLeVt6bfkArKcLBDRLl6ZsJ0ans5C-rSHr)", inline=True)
+            embed.add_field(name="Week 7", value="[Playlist](https://www.youtube.com/playlist?list=PLeVt6bfkArKdTo7GTqxO-kXXqhrRivXL9)", inline=True)
+            embed.add_field(name="Week 8", value="[Playlist](https://youtube.com/playlist?list=PLeVt6bfkArKfPpT1z_fE9PJZrF6Afwc4o&si=0zOi6YwAhKWBtS0W)", inline=True)
+            embed.add_field(name="Week 9", value="[Video](https://youtu.be/q2SGW2VgwAM?si=VDlPbv59jsc8M3TM)", inline=True)
+            embed.add_field(name="Week 10", value="[Playlist](https://youtube.com/playlist?list=PLeVt6bfkArKfLRcApQxz69y_q-lpS9HbK&si=bW5sysAnl7ewuvkL)", inline=True)
+
+        elif select.values[0] == "CO1105":
             embed.set_image(url="https://cdn.freebiesupply.com/logos/large/2x/java-logo-png-transparent.png")
             embed.add_field(name="Java Basics",value="[Link](https://www.codecademy.com/learn/java-introduction)", inline=True)
             embed.add_field(name="Introduction to OOP",value="[Link](https://www.codecademy.com/learn/learn-java-object-oriented-programming)", inline=True)
@@ -49,6 +66,8 @@ class Tutorials(discord.ui.View):
             embed.add_field(name="Week 4", value="[Video](https://youtu.be/m1Fjdnj_Mds)", inline=True)
             embed.add_field(name="Week 5", value="[Playlist](https://www.youtube.com/playlist?list=PLeVt6bfkArKcKV42o5N8ITcGxnPCjCHdH)", inline=True)
             embed.add_field(name="Week 6", value="[Video](https://youtu.be/JlMyYuY1aXU)", inline=True)
+            embed.add_field(name="Week 7", value="[Playlist](https://www.youtube.com/playlist?list=PLeVt6bfkArKdPM_kC0PWwlaG2-HswSa0g)", inline=True)
+            embed.add_field(name="Week 8", value="[Video](https://youtu.be/oSWTXtMglKE)", inline=True)
         elif select.values[0] == "CO2102":
             embed.set_image(url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Sql_data_base_with_logo.png/640px-Sql_data_base_with_logo.png")
             embed.add_field(name="Week 1", value="[Playlist](https://www.youtube.com/playlist?list=PLeVt6bfkArKcDa27fwSMqOb7_LnAMeAOI)")
@@ -56,23 +75,21 @@ class Tutorials(discord.ui.View):
             embed.set_image(url="https://cms-media.bartleby.com/wp-content/uploads/sites/2/2021/05/31175359/Theoretical-Computer-Science-1-1024x389.jpg")
             embed.add_field(name="Theory",value="WIP! If you find any resources that would be helpful. Please reach out!", inline=True)
         embed.set_footer(text="Bot created by: kaeini")
-        #Workaround for the DM function failing occasionally.
         await interaction.response.defer()
         user = interaction.user
-        #Sends user embed
         await user.send(embed=embed)
-        #Lets user know embed has sent.
         await interaction.followup.send(f"The material for {select.values[0]} has been DM'd to you {interaction.user.name}")
 
 
+
+
 async def tutorials(ctx):
-    try:
-        await ctx.defer()
-        message = await ctx.send("", view=Tutorials())
-        await ctx.followup.send("‎", delete_after=0.001)
-        await asyncio.sleep(15)
-        await message.delete()
-    except discord.NotFound:
-        print("No tutorials found")
+    await ctx.defer()
+    message = await ctx.send("", view=Tutorials())
+    await ctx.followup.send("‎", delete_after=0.001)
+    await asyncio.sleep(15)
+    await message.delete()
+
+
 
 
